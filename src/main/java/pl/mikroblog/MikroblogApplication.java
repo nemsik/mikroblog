c7b81102de0ca044fd431ce93ba6e7bb4553bd2c
bartek <barteknems@gmail.com>
2019-02-07 17:58:15 +0100

    init

---
+++ src/main/java/pl/mikroblog/MikroblogApplication.java
@@ -0,0 +1,14 @@
+package pl.mikroblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikroblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikroblogApplication.class, args);
+	}
+
+}
+
