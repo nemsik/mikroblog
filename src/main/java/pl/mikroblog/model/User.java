e7b82f4f6854e5e7e6e18e3d6967287c6d68582c
bartek <barteknems@gmail.com>
2019-02-07 18:38:32 +0100

    dodanie modeli user, post, comment

---
+++ src/main/java/pl/mikroblog/model/User.java
@@ -0,0 +1,32 @@
+package pl.mikroblog.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by bartek on 07.02.2019.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column (nullable = false, unique = true)
+    @Length (min = 3, message = "*Your nick must have at least 3 characters")
+    private String nick;
+
+    @Length(min = 5, message = "*Your password must have at least 5 characters")
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "user")
+    private Set<Post> comments = new HashSet<>();
+
+
+}
